5f763fe32e4931e172b5b47f0b9ff8d4e1f76af7
XnonCorp <faridbagusid@gmail.com>
2025-06-15 21:22:16 +0700

    fix: add async attribute to scripts to prevent synchronous script errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,16 +33,19 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         {children}
-        
-        {/* Bootstrap JS */}
+          {/* Bootstrap JS */}
         <script 
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
           integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
           crossOrigin="anonymous"
+          async
         />
         
         {/* Tabulator JS */}
-        <script src="https://unpkg.com/tabulator-tables@6.2.1/dist/js/tabulator.min.js" />
+        <script 
+          src="https://unpkg.com/tabulator-tables@6.2.1/dist/js/tabulator.min.js" 
+          async
+        />
       </body>
     </html>
   )
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -25,16 +25,19 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        
-        {/* Bootstrap JS */}
+          {/* Bootstrap JS */}
         <script 
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
           integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
           crossOrigin="anonymous"
+          async
         />
         
         {/* Tabulator JS */}
-        <script src="https://unpkg.com/tabulator-tables@6.2.1/dist/js/tabulator.min.js" />
+        <script 
+          src="https://unpkg.com/tabulator-tables@6.2.1/dist/js/tabulator.min.js" 
+          async
+        />
       </body>
     </Html>
   )
